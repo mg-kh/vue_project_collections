07feacf600c4aed500daa67bc3c9d86319da98cd
mg-kh <tragonn18@gmail.com>
2020-12-02 13:06:48 +0630

    page_title_change

--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,18 @@ const routes = [
     path: "/",
     name: "Hero",
     component: Hero,
+    meta: {
+      title: "To Do List",
+    },
   },
   {
     path: "/calculator",
     name: "Calculator",
     component: () =>
       import(/* webpackChunkName: "calculator" */ "@/views/Calculator.vue"),
+    meta: {
+      title: "Calculator",
+    },
   },
   {
     path: "/objector",
@@ -20,12 +26,18 @@ const routes = [
       import(
         /* webpackChunkName: "object dector" */ "@/views/ObjectDector.vue"
       ),
+    meta: {
+      title: "Object Dector",
+    },
   },
   {
     path: "/check_weather",
     name: "Check Weather",
     component: () =>
       import(/* webpackChunkName: "check weather" */ "@/views/Weather.vue"),
+    meta: {
+      title: "Check Weather",
+    },
   },
   {
     path: "/live_stream_video",
@@ -34,12 +46,20 @@ const routes = [
       import(
         /* webpackChunkName: "check weather" */ "@/views/LiveStreamVideo.vue"
       ),
+    meta: {
+      title: "Live Stream",
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
 });
 
 export default router;
